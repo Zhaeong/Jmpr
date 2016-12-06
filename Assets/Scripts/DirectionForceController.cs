37696f1b7d4930c5ac5c76f2a817208634401df5
Zhaeong <owen_zzz@hotmail.com>
2016-12-06 00:18:36 -0500

    added prelim addforcing

--- Assets/Scripts/DirectionForceController.cs
+++ Assets/Scripts/DirectionForceController.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class DirectionForceController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Rigidbody Object_RB;
+    public float Speed;
+
+    Vector3 vStartVector;
+    Vector3 vEndVector; 
+    // Use this for initialization
+    void Start () {
+        Object_RB = gameObject.GetComponent<Rigidbody>();
+        vStartVector = Vector3.zero;
+        vEndVector = Vector3.zero;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            vStartVector = Input.mousePosition;
+
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            vEndVector = Input.mousePosition;
+            Vector3 vDirection = vEndVector - vStartVector;
+            Object_RB.AddForce(vDirection * Speed);
+        }
+
+        Debug.Log("startvec:" + vStartVector + " Endvec:" + vEndVector);
+
+
+    }
+
+
 }
