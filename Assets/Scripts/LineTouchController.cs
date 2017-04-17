3e7f2428fef2986308127f099e9274f9f33c85ed
Zhaeong <owen_zzz@hotmail.com>
2017-04-16 20:33:02 -0400

    made limit on line drawing

--- Assets/Scripts/LineTouchController.cs
+++ Assets/Scripts/LineTouchController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LineTouchController : MonoBehaviour {
 
-    public GameObject LineObjStart, LineObjEnd;
+    public GameObject LineObjStart, LineObjEnd, LineObjMid;
     public Vector3 mousepos;
     public Vector3 worldPos;
 
@@ -37,15 +37,31 @@ public class LineTouchController : MonoBehaviour {
         {
             mousepos = Input.mousePosition;
 
-
             Vector3 WorldTransform = new Vector3(mousepos.x, mousepos.y, 3);
 
             worldPos = Camera.main.ScreenToWorldPoint(WorldTransform);
 
             LineObjEnd.transform.position = worldPos;
+            Debug.Log(Vector3.Distance(LineObjStart.transform.position, LineObjEnd.transform.position));
 
-            LR.SetPosition(0, LineObjStart.transform.position);
-            LR.SetPosition(1, LineObjEnd.transform.position);
+            if (Vector3.Distance(LineObjStart.transform.position, LineObjEnd.transform.position) > 1)
+            {
+                LineObjMid.transform.position = LineObjStart.transform.position + (LineObjEnd.transform.position - LineObjStart.transform.position).normalized;
+
+
+                LR.SetPosition(0, LineObjStart.transform.position);
+                LR.SetPosition(1, LineObjMid.transform.position);
+                LR.SetPosition(2, LineObjEnd.transform.position);
+            }
+            else
+            {
+                LineObjMid.transform.position = worldPos;
+                LR.SetPosition(0, LineObjStart.transform.position);
+                LR.SetPosition(1, LineObjMid.transform.position);
+                LR.SetPosition(2, LineObjEnd.transform.position);
+            }
+
+            
         }
         if (Input.GetMouseButtonUp(0))
         {
